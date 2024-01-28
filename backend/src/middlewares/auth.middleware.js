cc9b1b36afa674fa909d80f2511fe948555e0374
vikas singh <vikas7490@gmail.com>
2024-01-29 03:09:09 +0530

    changes

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
-    const { token } = req.cookies;
+    const { token } = req.cookies
     req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
       throw new ApiError(401, "unauthorized request");
